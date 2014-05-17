a2a31b7da607907b27bd0d22ac3e1917d107bbb6
zofrex <zofrex@gmail.com>
2014-05-17 16:14:03 +0100

    Make the example app actually launch

--- src/androidTest/java/com/example/templateproject/MyActivityTest.java
+++ src/androidTest/java/com/example/templateproject/MyActivityTest.java
@@ -1,14 +1,12 @@
+package com.example.templateproject;
+
 import android.app.Activity;
 import android.widget.TextView;
-
-import com.example.templateproject.R;
-
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,7 +15,6 @@ public class MyActivityTest {
     @Test
     public void testPlaceholderIsThere() {
         Activity sut = Robolectric.buildActivity(MyActivity.class).create().start().resume().get();
-
         TextView placeholder = (TextView) sut.findViewById(R.id.placeholder);
         assertEquals("Hello", placeholder.getText());
     }
--- src/main/java/com/example/templateproject/MyActivity.java
+++ src/main/java/com/example/templateproject/MyActivity.java
@@ -1,16 +1,12 @@
+package com.example.templateproject;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.templateproject.R;
 
 public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.mylayout);
     }
 }
